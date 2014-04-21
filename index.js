1955d7c554706baf9fdf0ee05fef360900fcbbd1
Scott Flesher <sfleshe@rei.com>
2014-04-21 16:56:51 -0700

    - implemented tie-in for metrics reporting.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var es = require("event-stream");
-var path = require("path");
+var es   = require( "event-stream" );
+var path = require( "path" );
+var lynx = require( "lynx" );
+
 "use strict";
 
-var istanbul = require("istanbul");
+var istanbul = require( "istanbul" );
+var utils    = require( "istanbul/lib/object-utils" );
+
 var hook = istanbul.hook;
 var Report = istanbul.Report;
 var Collector = istanbul.Collector;
@@ -13,44 +17,64 @@ var instrumenter = new istanbul.Instrumenter( {
 
 
 var plugin  = module.exports = function () {
-  var fileMap = {};
+    var fileMap = {};
 
-  hook.hookRequire(function (path) {
-    return !!fileMap[path];
-  }, function (code, path) {
-    return fileMap[path];
-  });
-
-  return es.map(function (file, cb) {
-    if (!file.contents instanceof Buffer) {
-      return cb(new Error("gulp-istanbul: streams not supported"), undefined);
-    }
-
-    instrumenter.instrument(file.contents.toString(), file.path, function (err, code) {
-      if (!err) file.contents = new Buffer(code);
-      fileMap[file.path] = file.contents.toString();
-      cb(err, file);
+    hook.hookRequire(function (path) {
+        return !!fileMap[path];
+    }, function (code, path) {
+        return fileMap[path];
     });
-  });
+
+    return es.map(function (file, cb) {
+        if (!file.contents instanceof Buffer) {
+            return cb(new Error("gulp-istanbul: streams not supported"), undefined);
+        }
+
+        instrumenter.instrument(file.contents.toString(), file.path, function (err, code) {
+            if (!err) file.contents = new Buffer(code);
+            fileMap[file.path] = file.contents.toString();
+            cb(err, file);
+        });
+    });
 };
 
-plugin.writeReports = function (dir) {
-  dir = dir || path.join(process.cwd(), "coverage");
+plugin.writeReports = function ( opts ) {
 
-  return es.through(null, function () {
-    var collector = new Collector();
+    var dir = opts.dir || path.join( process.cwd(), "coverage" );
+    var reportStats = opts.reportStats || false;
+    var statsHost   = opts.statsHost   || 'localhost';
+    var statsPort   = opts.statsPort   || 8125;
 
-    if ( global.__empty_coverage__ ) {
-        collector.add(global.__empty_coverage__);
-    }
-    collector.add(global.__coverage__);
+    return es.through( null, function () {
+        var collector = new Collector();
 
-    var reports = [
-        Report.create("html", { dir: dir }),
-        Report.create("text-summary")
-    ];
-    reports.forEach(function (report) { report.writeReport(collector, true); });
-    this.emit('end');
-  });
+        if ( global.__empty_coverage__ ) {
+            collector.add( global.__empty_coverage__ );
+        }
+        collector.add( global.__coverage__ );
 
+        var reports = [
+            Report.create( "html", { dir: dir } ),
+            Report.create( "text-summary" )
+        ];
+        reports.forEach( function ( report ) { report.writeReport( collector, true ); } );
+
+        if ( reportStats ) {
+            try {
+                var numberOfFiles = collector.files().length;
+                var summary = utils.summarizeCoverage( collector.getFinalCoverage() );
+
+                var metrics = new lynx( statsHost, statsPort );
+
+                metrics.gauge( 'fed.js.total_files', numberOfFiles );
+                metrics.gauge( 'fed.js.statements.covered', summary.statements.covered );
+                metrics.gauge( 'fed.js.statements.percent', summary.statements.pct );
+                metrics.gauge( 'fed.js.statements.total', summary.statements.total );
+            } catch ( e ) {
+                console.log( 'Unable to report statistics: ' + e );
+            }
+        }
+
+        this.emit( 'end' );
+    } );
 };
